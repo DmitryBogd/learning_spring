b347d7c3b20da3508d8772eb5d8d24dadd64cf9b
DmitryBogd <1999dima90@gmail.com>
2021-07-12 16:22:10 +0300

    Initial commit

---
+++ src/main/java/learning_spring/testSpring.java
@@ -0,0 +1,15 @@
+package learning_spring;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class testSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+
+        TestBean testBean = context.getBean("testBean", TestBean.class);
+        System.out.println(testBean.getName());
+        context.close();
+    }
+}
